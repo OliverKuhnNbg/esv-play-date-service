e9efa3965321a7294e9a575167db2115ccc3a6d4
Oliver <oliver.kuhn@ancud.de>
2023-11-15 14:21:04 +0100

    init spring project

---
+++ esv-game-service/src/main/java/de/twist/esvgameservice/EsvGameServiceApplication.java
@@ -0,0 +1,13 @@
+package de.twist.esvgameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsvGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsvGameServiceApplication.class, args);
+	}
+
+}
---
+++ esv-game-service/src/test/java/de/twist/esvgameservice/EsvGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.twist.esvgameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsvGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
