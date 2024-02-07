24f83d124ec7a707de11eeeabd6515a6c07d261c
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-02-07 19:01:13 +0100

    rename base app package & add 'controllers' sub package

--- esv-game-service/src/main/java/de/twist/esvgamesserviceapp/EsvPlayDateServiceApplication.java
+++ esv-game-service/src/main/java/de/twist/esvgamesserviceapp/EsvPlayDateServiceApplication.java
@@ -1,4 +1,4 @@
-package de.twist.esvgamesservice;
+package de.twist.esvgamesserviceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
