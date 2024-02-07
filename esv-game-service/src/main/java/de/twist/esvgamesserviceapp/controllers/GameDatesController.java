4393fdfe078dbfedb36562da2d5691c2c9e8aec5
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-02-07 20:42:56 +0100

    controller tested

--- esv-game-service/src/main/java/de/twist/esvgamesserviceapp/controllers/GameDatesController.java
+++ esv-game-service/src/main/java/de/twist/esvgamesserviceapp/controllers/GameDatesController.java
@@ -1,20 +1,17 @@
 package de.twist.esvgamesserviceapp.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+// @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class GameDatesController {
 
 	@GetMapping(value="/")
 	public String home() {
-		System.out.println("test");
-		
-		
+		System.out.println("\n\nhome() - controller called!");
 		return "home";
 	}
 }
